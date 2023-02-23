94db011f76ae9207cb4ac13308162c85f991bcd5
Adrian Ceapa <adrian.ceapa@gmail.com>
2023-02-23 15:19:24 +0200

    updates settings

--- lua/ad/settings.lua
+++ lua/ad/settings.lua
@@ -1,7 +1,9 @@
 local opt = vim.opt 
 vim.o.relativenumber = true
 vim.o.number = true
-vim.cmd('colorscheme rose-pine')
+vim.cmd.colorscheme('rose-pine')
+vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
 opt.clipboard = unnamedplus
 opt.scrolloff = 8
 opt.tabstop= 4
