6fe0f38c09937b770d4bc5b1a3b2b854ad9afe17
Adrian Ceapa <adrian.ceapa@gmail.com>
2023-02-27 21:23:42 +0200

    add lsp keybinding

--- lua/ad/remap.lua
+++ lua/ad/remap.lua
@@ -1,7 +1,27 @@
 vim.g.mapleader = " "
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<C-s>", ':w<CR>')
-vim.keymap.set("n", "<leader>y", '"+y')
-vim.keymap.set("v", "<leader>y", '"+y')
+local map = function(m, lhs, rhs)
+    local opts = {remap = false, silent = true}
+    vim.keymap.set(m, lhs, rhs, opts)
+end
+
+ -- LSP actions 
+map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+map('n', 'cd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+map('n', 'cD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+map('n', 'ci', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+map('n', 'ct', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+map('n', 'cr', '<cmd>lua vim.lsp.buf.references()<cr>')
+map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+map('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
+map('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+map('x', '<F4>', '<cmd>lua vim.lsp.buf.range_code_action()<cr>')
+-- Diagnostics 
+map('n', 'dl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+map('n', 'dp', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+map('n', 'dn', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+map("n", "<leader>pv", vim.cmd.Ex)
+map("n", "<C-s>", ':w<CR>')
+map("n", "<leader>y", '"+y')
+map("v", "<leader>y", '"+y')
