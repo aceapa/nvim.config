550c9a93352f9b44644db4ea132850398413de93
Adrian Ceapa <aceapa@users.noreply.github.com>
2023-03-06 12:22:03 +0200

    add new settings

--- lua/ad/settings.lua
+++ lua/ad/settings.lua
@@ -1,26 +1,44 @@
-local opt = vim.opt
 local o = vim.opt
+-- Highlight the region on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = num_au,
+    callback = function()
+        vim.highlight.on_yank({ higroup = "Visual", timeout = 120 })
+    end,
+})
+-- gui and colorscheme 
+--#region
 vim.cmd.colorscheme('rose-pine')
 vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
 vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
-opt.clipboard = 'unnamedplus'
-opt.scrolloff = 8
-opt.tabstop = 4
-opt.softtabstop = 4
-opt.shiftwidth = 4
-opt.smartindent = true
-opt.swapfile = false
-opt.backup = false
-opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-opt.undofile = true
-opt.incsearch = true
-opt.signcolumn = "yes"
-opt.termguicolors = true
-opt.updatetime = 50
-opt.colorcolumn = "80"
+o.termguicolors = true
+o.colorcolumn = "80"
+o.updatetime = 50
+--#endregion
+o.clipboard = 'unnamedplus'
+o.autoindent = true
+o.scrolloff = 8
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.swapfile = false
+o.backup = false
+o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+o.undofile = true
+--search
+o.hlsearch = false
+o.incsearch = true
 -- cursor settings: highlight relativenumber set color to pink
 vim.cmd('hi CursorLineNr guifg=#af00af')
 o.relativenumber = true
 o.number = true
 o.cursorlineopt = "number"
 o.cursorline = true
+o.signcolumn = "yes"
+o.smartindent = true
+-- display special characters
+o.list = true
+o.listchars = "trail:·,nbsp:◇,tab:→ ,extends:▸,precedes:◂,space:·,eol:¬"
+-- Case insensitive searching UNLESS /C or capital in search
+o.ignorecase = true
+o.smartcase = true
