0baffcf96b5452b41324529eb14fcad0c9c87f63
aceapa <adrian.ceapa@gmail.com>
2023-12-30 17:30:22 +0200

    replace tabs with spaces

--- lua/ad/packer.lua
+++ lua/ad/packer.lua
@@ -1,8 +1,8 @@
 vim.cmd [[packadd packer.nvim]]
 local status, packer = pcall(require, "packer")
 if not status then
-	print("Packer is not installed")
-	return
+    print("Packer is not installed")
+    return
 end
 
 -- Reloads Neovim after whenever you save packer.lua
@@ -14,48 +14,48 @@ vim.cmd([[
 ]])
 
 packer.startup(function(use)
-	use('wbthomason/packer.nvim')
-	use('folke/tokyonight.nvim')
-	use('rose-pine/neovim')
-	use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
-	-- use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v1.x',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
+    use('wbthomason/packer.nvim')
+    use('folke/tokyonight.nvim')
+    use('rose-pine/neovim')
+    use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
+    -- use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
 
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-buffer'},
-			{'hrsh7th/cmp-path'},
-			{'saadparwaiz1/cmp_luasnip'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'hrsh7th/cmp-nvim-lua'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'saadparwaiz1/cmp_luasnip'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
 
-			-- Snippets
-			{'L3MON4D3/LuaSnip'},
-			{'rafamadriz/friendly-snippets'},
-		}
-	}
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.4',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use {
-		'nvim-lualine/lualine.nvim',
-		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-	}
-	use("theprimeagen/harpoon")
-	use("theprimeagen/refactoring.nvim")
-	use("mbbill/undotree")
-	use("tpope/vim-fugitive")
-	use("folke/which-key.nvim")
-	use("mfussenegger/nvim-dap")
-	if packer_bootstrap then
-		packer.sync()
-	end
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
+        }
+    }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+    use("theprimeagen/harpoon")
+    use("theprimeagen/refactoring.nvim")
+    use("mbbill/undotree")
+    use("tpope/vim-fugitive")
+    use("folke/which-key.nvim")
+    use("mfussenegger/nvim-dap")
+    if packer_bootstrap then
+        packer.sync()
+    end
 end)
